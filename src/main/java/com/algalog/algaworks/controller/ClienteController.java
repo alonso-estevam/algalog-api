33015c90066c7b58c932be6762ceb678923b28e0
alonso-estevam <alonso_estevam@outlook.com>
2022-11-04 19:00:57 -0300

    feat: add endpoint for get client id

--- src/main/java/com/algalog/algaworks/controller/ClienteController.java
+++ src/main/java/com/algalog/algaworks/controller/ClienteController.java
@@ -1,26 +1,33 @@
 package com.algalog.algaworks.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algalog.algaworks.domain.model.Cliente;
+import com.algalog.algaworks.domain.repository.ClienteRepository;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 @RestController
 public class ClienteController {
-	
-	@PersistenceContext
-	private EntityManager manager;
-	
+		
+	private ClienteRepository clienteRepository;
+
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		return manager.createQuery("from Cliente", Cliente.class)
-				.getResultList();
+		return clienteRepository.findAll();
+	}
+	
+	@GetMapping("/clientes/{clienteId}")
+	public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) {
+		return clienteRepository.findById(clienteId)
+				.map(ResponseEntity::ok)
+				.orElse(ResponseEntity.notFound().build());
 	}
 	
 }
--- src/main/java/com/algalog/algaworks/domain/repository/ClienteRepository.java
+++ src/main/java/com/algalog/algaworks/domain/repository/ClienteRepository.java
@@ -1,9 +1,13 @@
 package com.algalog.algaworks.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.algalog.algaworks.domain.model.Cliente;
 
+@Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long>{
-
+	
 }
