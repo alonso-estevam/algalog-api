1e3bd22dabdabcc5db0c8ef08cfb01fa443ed853
alonso-estevam <alonso_estevam@outlook.com>
2022-11-04 20:10:15 -0300

    feat: add endpoint to post clients

--- src/main/java/com/algalog/algaworks/controller/ClienteController.java
+++ src/main/java/com/algalog/algaworks/controller/ClienteController.java
@@ -2,10 +2,14 @@ package com.algalog.algaworks.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algalog.algaworks.domain.model.Cliente;
@@ -32,4 +36,9 @@ public class ClienteController {
 				.orElse(ResponseEntity.notFound().build());
 	}
 	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente adicionar(@RequestBody Cliente cliente) {
+		return clienteRepository.save(cliente);
+	}
 }
