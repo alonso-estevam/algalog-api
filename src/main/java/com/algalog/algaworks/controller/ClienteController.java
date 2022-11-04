be662b0b7c11fd98cff61d7451bf72c90efba0cd
alonso-estevam <alonso_estevam@outlook.com>
2022-11-04 19:04:14 -0300

    refactor: add request mapping annotation

--- src/main/java/com/algalog/algaworks/controller/ClienteController.java
+++ src/main/java/com/algalog/algaworks/controller/ClienteController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algalog.algaworks.domain.model.Cliente;
@@ -14,16 +15,17 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 @RestController
+@RequestMapping("/clientes")
 public class ClienteController {
 		
 	private ClienteRepository clienteRepository;
 
-	@GetMapping("/clientes")
+	@GetMapping
 	public List<Cliente> listar() {
 		return clienteRepository.findAll();
 	}
 	
-	@GetMapping("/clientes/{clienteId}")
+	@GetMapping("/{clienteId}")
 	public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) {
 		return clienteRepository.findById(clienteId)
 				.map(ResponseEntity::ok)
