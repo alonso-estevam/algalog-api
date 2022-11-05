0184f33423ddb664e464d67a79e28800dbf87227
alonso-estevam <alonso_estevam@outlook.com>
2022-11-05 14:38:38 -0300

    feat: add <Entrega> class

---
+++ src/main/java/com/algalog/algaworks/domain/model/StatusEntrega.java
@@ -0,0 +1,7 @@
+package com.algalog.algaworks.domain.model;
+
+public enum StatusEntrega {
+	
+	PENDENTE, FINALIZADA, CANCELADA
+	
+}
---
+++ src/main/java/com/algalog/algaworks/domain/repository/EntregaRepository.java
@@ -0,0 +1,11 @@
+package com.algalog.algaworks.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.algalog.algaworks.domain.model.Entrega;
+
+@Repository
+public interface EntregaRepository extends JpaRepository<Entrega, Long> {
+
+}
