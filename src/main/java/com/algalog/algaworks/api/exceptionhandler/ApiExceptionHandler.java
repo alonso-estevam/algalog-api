d79609310c8e0c8e11f126caae0b232aa3f253c1
alonso-estevam <alonso_estevam@outlook.com>
2022-11-07 18:15:58 -0300

    refactor: exception handler for <Entrega> not found

--- src/main/java/com/algalog/algaworks/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algalog/algaworks/api/exceptionhandler/ApiExceptionHandler.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import com.algalog.algaworks.domain.exception.EntidadeNaoEncontradaException;
 import com.algalog.algaworks.domain.exception.NegocioException;
 
 import lombok.AllArgsConstructor;
@@ -52,6 +53,18 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 	@ExceptionHandler
 	public ResponseEntity<Object> handleNegocio(NegocioException ex, WebRequest request){
 		HttpStatus status = HttpStatus.BAD_REQUEST;
+		
+		Problema problema = new Problema();
+		problema.setStatus(status.value());
+		problema.setDataHora(OffsetDateTime.now());
+		problema.setTitulo(ex.getMessage());
+		
+		return handleExceptionInternal(ex, problema, new HttpHeaders(), status, request);
+	}
+	
+	@ExceptionHandler(EntidadeNaoEncontradaException.class)
+	public ResponseEntity<Object> handleEntidadeNaoEncontrada(EntidadeNaoEncontradaException ex, WebRequest request){
+		HttpStatus status = HttpStatus.NOT_FOUND;
 		
 		Problema problema = new Problema();
 		problema.setStatus(status.value());
