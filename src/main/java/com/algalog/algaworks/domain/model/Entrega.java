ce42af13b897115f4046e71778e5b9c28cbcde7f
alonso-estevam <alonso_estevam@outlook.com>
2022-11-07 10:48:36 -0300

    refactor: change date type to offset

--- src/main/java/com/algalog/algaworks/domain/model/Entrega.java
+++ src/main/java/com/algalog/algaworks/domain/model/Entrega.java
@@ -2,6 +2,7 @@ package com.algalog.algaworks.domain.model;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
@@ -54,9 +55,9 @@ public class Entrega {
 	private StatusEntrega status;
 	
 	@JsonProperty(access = Access.READ_ONLY)
-	private LocalDateTime dataPedido;
+	private OffsetDateTime dataPedido;
 	
 	@JsonProperty(access = Access.READ_ONLY)
-	private LocalDateTime dataFinalizacao;
+	private OffsetDateTime dataFinalizacao;
 	
 }
